9f982594c65cb420c5f814d8392c252a353aac79
lacourligc <120428481+lacourligc@users.noreply.github.com>
2023-01-05 18:07:14 -0300

    Update app.js

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/home.html'));
+});
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/register.html'));
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/login.html'));
+});
+const port = process.env.PORT || 3001;
+ app.listen(port, () => {   
+  console.log('Servidor iniciado en http://localhost:' + port); 
+});
